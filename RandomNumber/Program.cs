a4e571283db4eba03da2b9119c802ac72c59e6e1
Leo Taylor <leotaylor723@gmail.com>
2018-09-14 10:57:08 -0500

    prints random numbers, squares them, removes odd numbers

--- RandomNumber/Program.cs
+++ RandomNumber/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RandomNumber
 {
@@ -6,7 +7,52 @@ namespace RandomNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rand = new Random();
+
+            var Numbers = new List<int>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                Numbers.Add(rand.Next(1, 51));
+            }
+
+            Console.WriteLine("20 Random Numbers");
+            foreach(int randomNumber in Numbers)
+            {
+                Console.WriteLine(randomNumber);
+            }
+            Console.ReadLine();
+
+            // Squared Root
+
+            Console.WriteLine("Sqaured Root of Numbers");
+            var squared = new List<int>();
+            foreach(int number in Numbers)
+            {
+                var squareRoot = number * number;
+                squared.Add(squareRoot);
+                Console.WriteLine($"{number} : {squareRoot}");
+            }
+            Console.ReadLine();
+
+            // even numbers
+
+            var even = new List<int>();
+
+            foreach(int squareNumber in squared)
+            {
+                if (squareNumber % 2 == 0)
+                {
+                    even.Add(squareNumber);
+                }
+            }
+            Console.WriteLine("Only Even:");
+            foreach(int squareNumber in even)
+            {
+                Console.WriteLine(squareNumber);
+            }
+
+            Console.ReadLine();
         }
     }
 }
